7b6c090835f815b23a705085694dffb23100170f
lea-dubois1 <lea.dubois@laplateforme.io>
2024-04-02 11:33:50 +0200

    job 1 terminé

---
+++ jour01/job01/job01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello Word!\n";
+    return 0;
+}
+
+// OR
+
+// #include <iostream>
+
+// int main()
+// {
+//     std::cout << "Hello Word!\n";
+//     return 0;
+// }
